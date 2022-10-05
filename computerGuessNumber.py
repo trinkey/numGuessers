7e6e1c3c9f0fb0afa7696241e70d654d8a00d961
trinkey_ <97406176+trinkey@users.noreply.github.com>
2022-10-05 07:20:49 -0400

    Update computerGuessNumber.py

--- computerGuessNumber.py
+++ computerGuessNumber.py
@@ -1,11 +1,17 @@
 maximum = 100
 minimum = 0
+randomMode = True
+if randomMode:
+    import random
 hlc = ""
 guess2 = maximum + 1
 input("To play this game, I will ask you if <insert number here> is your number.\nIf your number is higher than the guess, then input h.\nIf lower, input l.\nAnd if my guess is correct, input c.\n\nThink of a WHOLE NUMBER between " + str(minimum) + " and " + str(maximum) + " and then press enter.\n")
 while hlc != "c":
     hlc = ""
-    guess = round((minimum + maximum) / 2)
+    if randomMode:
+        try: guess = random.randint(minimum, maximum)
+        except ValueError: print("Either me or you messed up. Probably you."); break
+    else: guess = round((minimum + maximum) / 2)
     if guess2 == guess: print("Either me or you messed up. Probably me."); break
     guess2 = guess
     while hlc != "h" and hlc != "l" and hlc != "c": hlc = input("Is your number " + str(guess) + "?\n").lower()
